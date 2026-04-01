f4b338c1664443222a226e9b3daf9bf964f73cb8
flamengo1895 <wmengao980@gmail.com>
2026-04-01 20:55:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMod() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  
+
+
+  // substituir a imagem
+  // se tiver light mode. adicionar a imagem light
+  // se tiver sem light mode, manter a imagem normal
+}
